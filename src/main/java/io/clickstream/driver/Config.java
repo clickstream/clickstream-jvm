0dcac37c973d8d3d2e6ef233fb2689b1abf6675e
Jerome Touffe-Blin <jtouffeblin@expedia.com>
2013-12-27 23:32:11 +1100

    Fix escaping of pipe (|) in `split` regex string arg

--- src/main/java/io/clickstream/driver/Config.java
+++ src/main/java/io/clickstream/driver/Config.java
@@ -73,7 +73,7 @@ public class Config {
     private String getJsFilter(String param) {
         String sFilter = filterConfig.getInitParameter(param);
         return sFilter != null ?
-                "[" + join(sFilter.split("|"), ",") + "]" :
+                "[" + join(sFilter.split("\\|"), ",") + "]" :
                 null;
     }
 
